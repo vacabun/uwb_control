0063224fbc29304af13ae733c2aa1da06b356f72
guotong ma <maguotong66@gmail.com>
2024-03-21 14:49:00 +0800

    update

--- launch/controller_test_sim.launch.py
+++ launch/controller_test_sim.launch.py
@@ -4,64 +4,55 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     ld = LaunchDescription()
-    # node1 = Node(
-    #     package='uwb_ros_bridge_sim',
-    #     executable='bridge',
-    #     name='bridge_1',
-	#     output='screen',
-    #     parameters=[{'label_name': "x500_1"}]
-    # )
-    # ld.add_action(node1)
-
-    # node2 = Node(
-    #     package='uwb_ros_bridge_sim',
-    #     executable='bridge',
-    #     name='bridge_2',
-	#     output='screen',
-    #     parameters=[{'label_name': "x500_2"}]
-    # )
-    # ld.add_action(node2)
-    
-    # node3 = Node(
-    #     package='uwb_ros_bridge_sim',
-    #     executable='bridge',
-    #     name='bridge_3',
-	#     output='screen',
-    #     parameters=[{'label_name': "x500_3"}]
-    # )
-    # ld.add_action(node3)
 
     brigde_service = '/x500_1/uwb_bridge'
-    node4 = Node(
+    node1 = Node(
         package='uwb_control',      # 节点的包名
         executable='controller',     # 节点的可执行文件名
         name='uwb_controller_1',                # 节点的名称
         output='screen',              # 输出日志到屏幕
         parameters=[{'brigde_service': brigde_service}]
     )
-    ld.add_action(node4)
+    ld.add_action(node1)
 
     brigde_service = '/x500_2/uwb_bridge'
-    node5 = Node(
+    node2 = Node(
         package='uwb_control',      # 节点的包名
         executable='controller',     # 节点的可执行文件名
         name='uwb_controller_2',                # 节点的名称
         output='screen',              # 输出日志到屏幕
         parameters=[{'brigde_service': brigde_service}]
     )
-    ld.add_action(node5)
+    ld.add_action(node2)
 
     brigde_service = '/x500_3/uwb_bridge'
-    node6 = Node(
+    node3 = Node(
         package='uwb_control',      # 节点的包名
         executable='controller',     # 节点的可执行文件名
         name='uwb_controller_3',                # 节点的名称
         output='screen',              # 输出日志到屏幕
         parameters=[{'brigde_service': brigde_service}]
     )
-    ld.add_action(node6)
+    ld.add_action(node3)
 
+    brigde_service = '/x500_4/uwb_bridge'
+    node4 = Node(
+        package='uwb_control',      # 节点的包名
+        executable='controller',     # 节点的可执行文件名
+        name='uwb_controller_4',                # 节点的名称
+        output='screen',              # 输出日志到屏幕
+        parameters=[{'brigde_service': brigde_service}]
+    )
+    ld.add_action(node4)
 
-
+    brigde_service = '/x500_5/uwb_bridge'
+    node5 = Node(
+        package='uwb_control',      # 节点的包名
+        executable='controller',     # 节点的可执行文件名
+        name='uwb_controller_5',                # 节点的名称
+        output='screen',              # 输出日志到屏幕
+        parameters=[{'brigde_service': brigde_service}]
+    )
+    ld.add_action(node5)
 
     return ld
